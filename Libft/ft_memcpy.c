c4a3037b57e894a88c1923cf85a56d3be8cb3a2a
Ozan <92795767+Ozaaaan@users.noreply.github.com>
2023-10-24 14:15:42 +0200

    Add files via upload

---
+++ Libft/ft_memcpy.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozdemir <ozdemir@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 11:48:22 by ozdemir           #+#    #+#             */
+/*   Updated: 2023/10/20 16:49:31 by ozdemir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+#include <stdio.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t		i;
+	char		*destination;
+	const char	*source;
+
+	destination = dest;
+	source = src;
+	i = 0;
+	if (!dest && !src)
+		return (NULL);
+	while (i < n)
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char source[] = "Salut";
+	char destination[20];
+
+	ft_memcpy(destination, source, sizeof(source));
+	printf("Source : %s\n", source);
+	printf("Destination : %s\n", destination);
+	return (0);
+}*/
